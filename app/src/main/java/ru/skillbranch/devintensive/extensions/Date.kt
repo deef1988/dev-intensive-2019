94455e1874b97b01b840b67b11fa79cc322285d3
deef1988 <def1988@inbox.ru>
2019-07-01 16:33:24 +0300

    add DateTime function add and format

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,2 +1,35 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(valuse: Int, units: TimeUnits = TimeUnits.SECOND): Date {
+    var time = this.time
+
+    time +=when (units) {
+        TimeUnits.SECOND -> valuse * SECOND
+        TimeUnits.MINUTE -> valuse * MINUTE
+        TimeUnits.HOUR -> valuse * HOUR
+        TimeUnits.DAY -> valuse * DAY
+    }
+    this.time = time
+    return this
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
