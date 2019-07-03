73505e23115c4afda5a43f3c66dba0d9be3fc48b
deef1988 <def1988@inbox.ru>
2019-07-03 10:13:22 +0300

    add pattern Builder from user

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,6 +1,5 @@
 package ru.skillbranch.devintensive.models
 
-import android.provider.ContactsContract
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
@@ -11,7 +10,7 @@ data class User(
     var avatar: String?,
     var rating: Int = 0,
     var respect: Int = 0,
-    var lastVisit: Date = Date(),
+    var lastVisit: Date? = Date(),
     var isOnline: Boolean = false
 ) {
     constructor(id: String, firstName: String?, lastName: String?) : this(
@@ -51,4 +50,39 @@ data class User(
             isOnline: $isOnline
         """.trimIndent()
     )
+
+    //Паттерна Builder
+    data class Builder(
+        var id: String = "",
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var avatar: String? = null,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        var lastVisit: Date? = null,
+        var isOnline: Boolean = false
+    ) {
+
+
+
+        init {
+            lastId++
+            this.id = lastId.toString()
+        }
+
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+
+        companion object BID {
+            private var lastId = -1
+        }
+    }
 }
