31b40dee5e979e8e3cae9c1c93492bb8ea935e7b
deef1988 <def1988@inbox.ru>
2019-07-12 09:02:44 +0300

    drop fun stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -7,18 +7,29 @@ fun String.truncate(string: String, len: Int = 16): String {
     if (result.length >= len) {
         result = result.dropLast(result.length - len)
         result = drop_space_end(result)
-        result = result.padEnd(result.length+3,'.')
+        result = result.padEnd(result.length + 3, '.')
     }
     return result
 }
-//fun sech_pass(string: String): String {
-//    for ((i, symbol) in string.withIndex() downTo 1) {
-//
+
+
+//fun String.stripHtml(html: String): String {
+//    var result = ""
+//    var drop_html = fale
+//    for ((ind, car) in html.withIndex()) {
+//        if (drop_html == true) {
+//            //удаяем лишнии символы и проверяем не конец ли тега
+//        } else{
+//            //проверяем нет ли начала тега
+//        }
 //    }
+//
+//    return result
 //}
-fun drop_space_end(string: String): String{
+
+fun drop_space_end(string: String): String {
     var result = string
-    for (symbol in string){
+    for (symbol in string) {
         if (result.takeLast(1) == " ") {
 //            result = result.substring(0, result.length - 1)
             result = result.dropLast(1)
